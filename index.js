054ab3e934ed6cc9d9f52acd924d69c51abe9774
romasrs <shaman.roman@tut.by>
2020-10-16 01:11:07 +0300

    refactor: rename variables for better readability fix bugs

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const autocomplete = document.querySelector('.autocomplete');
 const input = autocomplete.firstElementChild;
-const records = document.querySelector('.wrapper-records');
+const records = document.querySelector('.record-wrapper');
 let listsCreate = false;
 
 const debounce = (fn, debounceTime) => {
@@ -20,25 +20,24 @@ const debounce = (fn, debounceTime) => {
 }
 
 const debouncedSearch = debounce(search, 500);
-input.addEventListener('input', debouncedSearch);
 
 async function search() {
+
+  const BASE_URL = `https://api.github.com/search/repositories?q=${input.value}&sort=stars&order=desc`;
+  const quantityOfoResponses = 5;
+  let response = await fetch(BASE_URL);
+  let result = await response.json();
+  let countOfHints = result.items.length;
 
   if (!input.value) {
     clearList()
     return;
   }
-    
-  const BASE_URL = `https://api.github.com/search/repositories?q=${input.value}&sort=stars&order=desc`;
-  const quantityOfoResponses = 5;
-  let response = await fetch(BASE_URL);
-  let result = await response.json();
 
-  let countOfHints = result.items.length;
   countOfHints = (countOfHints > quantityOfoResponses) ? quantityOfoResponses : countOfHints;
 
   if (!listsCreate) createLists(countOfHints)
-  let lists = document.querySelectorAll('.autocomplete-list');
+  let lists = document.querySelectorAll('.autocomplete__list');
 
   let listsArray = Array.from(lists);
   for (let i = 0; i < countOfHints; i++) {
@@ -58,53 +57,55 @@ function clearList(){
   listsCreate = false;
 }
 
+function addRecord(evt) {
+  
+  let data = evt.target.repoDate;
+  let record = document.createElement('li');
+  let name = `Name: ${data.name}`;
+  let owner = `Owner: ${data.owner.login}`;
+  let stars = `Stars: ${data.stargazers_count}`;
+  let arr = [name, owner, stars],
+  container = record.querySelector('.record-wrapper__list');
+  record.className = 'record-wrapper__list';
+
+  // record.insertAdjacentHTML('afterBegin', `<p class = "info">Name: ${data.name}</p>
+  //                                          <p class = "info">Owner: ${data.owner.login}</p>
+  //                                          <p class = "info">Stars: ${data.stargazers_count}</p>`);
+
+
+  arr.forEach((text) => {
+    const info = document.createElement('p');
+    info.textContent = text;
+    info.className = 'record-wrapper__info';
+    record.appendChild(info);
+  });
+
+
+  addButtonDelete(record);
+  records.append(record);
+  input.value = '';
+  setTimeout(clearList, 300);
+}
+
+input.addEventListener('input', debouncedSearch);
+
 function createLists(count){
   
   for (let i = 0; i < count; i++) {
     let list = document.createElement('li');
-    list.className = 'autocomplete-list';
+    list.className = 'autocomplete__list';
     list.addEventListener('click', addRecord);
     autocomplete.append(list);
     listsCreate = true;
   }
 }
 
-function addRecord(evt) {
-  
-  let data = evt.target.repoDate;
-  let record = document.createElement('div');
-  record.className = 'record';
-  // record.insertAdjacentHTML('afterBegin', `<p class = "info">Name: ${data.name}</p>
-  //                                          <p class = "info">Owner: ${data.owner.login}</p>
-  //                                          <p class = "info">Stars: ${data.stargazers_count}</p>`);
-
-
-  let name = `Name: ${data.name}`;
-  let owner = `Owner: ${data.owner.login}`;
-  let stars = `Stars: ${data.stargazers_count}`;
-  let arr = [name, owner, stars],
-  container = record.querySelector('.record');
-
-  arr.forEach((text) => {
-    const info = document.createElement('p');
-    info.textContent = text;
-    info.className = 'info';
-    record.appendChild(info);
-  });
-
-
-  addDelete(record);
-  records.append(record);
-  input.value = '';
-  setTimeout(clearList, 300);
-}
-
-function addDelete(elem) {
-  let parentDelete = document.createElement('button');
-  parentDelete.className = 'close';
-  parentDelete.addEventListener('click', () => {
+function addButtonDelete(elem) {
+  let deleteButtonClickHandler = document.createElement('button');
+  deleteButtonClickHandler.className = 'record-wrapper__btn-close';
+  deleteButtonClickHandler.addEventListener('click', () => {
     elem.remove();
   })
 
-  elem.append(parentDelete);
+  elem.append(deleteButtonClickHandler);
 }
